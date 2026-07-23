7548359d47e6c116ca2b242434d7c29ef81df7ea
Davi Machado Gomes <professional.damago1110@gmail.com>
2026-07-23 10:07:32 -0300

    refactor: clean up redudant code and files
    - Refactor config/logger_config.py to use settings instead of dotenv
    - Remove redundant load_dotenv() from config/logger_config.py
    - Delete duplicated db_connection.py and logger_config.py from root

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,14 +1,10 @@
 import os
 import logging
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-env_log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+import settings
 
 logging.basicConfig(
-    level=getattr(logging, env_log_level, logging.INFO),
+    level=getattr(logging, settings.LOG_LEVEL, logging.INFO),
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,5 @@
 import os
 
-import dotenv
 from dotenv import load_dotenv
 
 load_dotenv()
