690a293f7356e9b4d96934161f2d039a7e8c592e
Davi Machado Gomes <professional.damago1110@gmail.com>
2026-07-28 13:40:08 -0300

    feat: setup fastapi server and initial user endpoints
    - Configure Uvicorn server in main.py
    - Add health check route
    - Create POST /users endpoint for user registration

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+from fastapi import FastAPI
+
+from config.logger import logger
 from database.db_connection import get_supabase_connection
 from services.cloud_connection import get_bucket_client
-from config.logger import logger
+from routers import users
 
-def main():
-    return None
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+app.include_router(
+    users.router
+)
+
+@app.get("/")
+def health_check():
+    return {
+        "status": "API Online"
+    }
+
+# def main():
+#     return
+
+# if __name__ == "__main__":
+#     main()
---
+++ routers/users.py
@@ -0,0 +1,43 @@
+# from uuid import UUID
+
+from fastapi import APIRouter, Depends # HTTPException
+from pydantic import BaseModel
+
+from database.db_connection import get_supabase_connection
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"]
+)
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+# @router.get("/{user_id}")
+# def get_user_data(
+#     user_id: UUID,
+#     supabase = Depends(get_supabase_connection)
+# ):
+
+#     response = supabase.from_('users').select('id').eq('id', str(user_id)).execute()
+
+#     if not response.data:
+#         raise HTTPException(status_code=404, detail="User not found")
+
+#     return response.data[0]
+
+@router.post("/")
+def post_user_data(
+    user: UserCreate,
+    supabase = Depends(get_supabase_connection)
+):
+
+    response = supabase.from_('users').insert({
+        "name": user.name,
+        "email": user.email
+        }).execute()
+
+    new_user = response.data[0]
+
+    return new_user
